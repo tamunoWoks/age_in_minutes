a11c4529a3b3a5eadc191cd69e40e856945b3b39
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-08-04 21:35:32 -0600

    Exit the program with an error message if input is invalid

--- age.py
+++ age.py
@@ -10,6 +10,10 @@ def main():
         # Check if the date of birth is not in the future
         if dob > date.today():
             raise ValueError("Date of Birth cannot be in the future.")
+        
+    except ValueError as e:
+        # Exit the program with an error message if input is invalid
+        sys.exit(f"Invalid Input: {e}")
 
 if __name__ == 'main':
     main()
