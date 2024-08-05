aabd635eddbeb5050ccb855bdcb0fbdeef9da1f0
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-08-04 21:51:06 -0600

    Create test function and test cases with expected results

--- test_age.py
+++ test_age.py
@@ -1,2 +1,9 @@
 from age import get_collective_mins
 from datetime import date
+
+def test_get_mins():
+     # Test cases with expected results
+    assert get_collective_mins(date(1999, 1, 1), date(2000, 1, 1)) == 525600
+    assert get_collective_mins(date(2001, 1, 1), date(2003, 1, 1)) == 1051200
+    assert get_collective_mins(date(1995, 1, 1), date(2000, 1, 1)) == 2629440
+    assert get_collective_mins(date(2020, 6, 1), date(2032, 1, 1)) == 6092640
