992dff69f77784bd86280e4eda93f9075a9b81d6
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-08-04 21:32:40 -0600

    Prompt user for date of birth in YYYY-MM-DD format

--- age.py
+++ age.py
@@ -3,7 +3,8 @@ import inflect
 import sys
 
 def main():
-
+    # Prompt user for date of birth in YYYY-MM-DD format
+        dob = date.fromisoformat(input("Enter your Date of Birth (YYYY-MM-DD): "))
 
 if __name__ == 'main':
     main()
