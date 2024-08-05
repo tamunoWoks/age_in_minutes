7e34f63656f9845546c06652813977237d6ce9b2
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-08-04 21:34:35 -0600

    Check if the date of birth is not in the future

--- age.py
+++ age.py
@@ -3,8 +3,13 @@ import inflect
 import sys
 
 def main():
-    # Prompt user for date of birth in YYYY-MM-DD format
+    try:
+        # Prompt user for date of birth in YYYY-MM-DD format
         dob = date.fromisoformat(input("Enter your Date of Birth (YYYY-MM-DD): "))
+
+        # Check if the date of birth is not in the future
+        if dob > date.today():
+            raise ValueError("Date of Birth cannot be in the future.")
 
 if __name__ == 'main':
     main()
