df5446338da221c2139f76c23f8572f77716b9a8
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-08-04 21:56:37 -0600

    cleaned up code

--- test_age.py
+++ test_age.py
@@ -1,8 +1,9 @@
 from age import get_collective_mins
 from datetime import date
 
+
 def test_get_mins():
-     # Test cases with expected results
+    # Test cases with expected results
     assert get_collective_mins(date(1999, 1, 1), date(2000, 1, 1)) == 525600
     assert get_collective_mins(date(2001, 1, 1), date(2003, 1, 1)) == 1051200
     assert get_collective_mins(date(1995, 1, 1), date(2000, 1, 1)) == 2629440
